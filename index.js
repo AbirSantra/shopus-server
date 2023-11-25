495e1c108d686a0685d08f6d1af0b8f7beb41cd5
Abir Santra <santraabir8@gmail.com>
2023-11-25 19:07:28 +0530

    added database connection

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const connectDb = require("./db");
 
 /* App Initialization */
 const app = express();
 dotenv.config();
+connectDb();
 const PORT = process.env.PORT || 5000;
 
 /* Middlewares */
@@ -11,6 +14,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* Listeners */
-app.listen(PORT, () => {
-  console.log("Server listening on PORT: ", PORT);
+mongoose.connection.once("open", () => {
+  console.log("Connected to database!");
+  app.listen(PORT, () => {
+    console.log("Server started on PORT: ", PORT);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongo error: ", err);
 });
